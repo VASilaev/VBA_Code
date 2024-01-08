38b2778b1e4b75b9fad9b1ea0661c0d14774dbd5
VASilaev <swa111@yandex.ru>
2024-01-08 14:09:33 +0300

    Update VBA_DB_Date.vb

--- VBA_DB_Date.vb
+++ VBA_DB_Date.vb
@@ -17,3 +17,11 @@ Public Function MinDate(Optional dDate = Null) As Date
   MinDate = dDate
  End If
 End Function
+
+
+Public Function bitAnd(o1 As Long, o2 As Long) As Long
+'Выполняет побитовое "И"
+'#param o1: Первый операнд
+'#param o2: Второй операнд
+ bitAnd = o1 And o2
+End Function
